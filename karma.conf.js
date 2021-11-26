d79317dce55170f5bf9a08e6a69a758e5adcf5f6
veliohart <velen.web@gmail.com>
2021-11-26 10:10:24 +0200

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/new'),
+      dir: require('path').join(__dirname, './coverage/3commas-stats'),
       subdir: '.',
       reporters: [
         { type: 'html' },
